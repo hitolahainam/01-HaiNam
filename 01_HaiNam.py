8edbc7d8c72453231f905b1512cdce8497bf6c22
hitolahainam <namhai9210@gmail.com>
2025-10-20 17:32:47 +0700

    feat: implement add student function

---
+++ 01_HaiNam.py
@@ -0,0 +1,9 @@
+# student_manager.py
+def add_student(name, year_of_birth, address):
+    student = {
+        "name": name,
+        "year_of_birth": year_of_birth,
+        "address": address
+    }
+    student_list.append(student)
+    print(f"Da them sinh vien {name} thanh cong.")
