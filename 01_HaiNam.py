fe3ac1a9d96d299a18dbb06ef9f0c748b90377f7
hitolahainam <namhai9210@gmail.com>
2025-10-20 17:42:16 +0700

    feat: implement print student list

--- 01_HaiNam.py
+++ 01_HaiNam.py
@@ -7,3 +7,11 @@ def add_student(name, year_of_birth, address):
     }
     student_list.append(student)
     print(f"Da them sinh vien {name} thanh cong.")
+
+def print_student_list():
+    print("--- DANH SACH SINH VIEN ---")
+    if not student_list:
+        print("Danh sach trong.")
+    else:
+        for s in student_list:
+            print(f" - Ten: {s['name']}, Nam sinh: {s['year_of_birth']}, Dia chi: {s['address']}")
